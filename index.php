25904ec929d1a65f3b32beb3fba42f6a79e0839a
Basay <Basay1980@gmail.com>
2021-07-22 22:10:12 +0300

    Сделал покрасивей

--- index.php
+++ index.php
@@ -96,18 +96,18 @@ $show_complete_tasks = rand(0, 1);
                     </tr>
                     <!--показывать следующий тег <tr/>, если переменная $show_complete_tasks равна единице-->
                     <?php if ($show_complete_tasks==1): ?>
-<tr class="tasks__item task task--completed">
-                                                              <td class="task__select">
-    <label class="checkbox task__checkbox">
-      <input class="checkbox__input visually-hidden" type="checkbox" checked>
-      <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
-    </label>
-  </td>
-  <td class="task__date">10.10.2019</td>
+                        <tr class="tasks__item task task--completed">
+                            <td class="task__select">
+                                <label class="checkbox task__checkbox">
+                                    <input class="checkbox__input visually-hidden" type="checkbox" checked>
+                                    <span class="checkbox__text">Записаться на интенсив "Базовый PHP"</span>
+                                </label>
+                            </td>
+                            <td class="task__date">10.10.2019</td>
 
-  <td class="task__controls">
-  </td>
-</tr>'
+                            <td class="task__controls">
+                            </td>
+                      </tr>'
                     <?php endif; ?>
                 </table>
             </main>
