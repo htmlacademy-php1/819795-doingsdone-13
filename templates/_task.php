ad700210ba11bbd2d3f1ef73930f98da226722ed
Basay <Basay1980@gmail.com>
2021-07-28 19:19:15 +0300

    Шаблоны

--- templates/_task.php
+++ templates/_task.php
@@ -2,10 +2,10 @@
     <td class="task__select">
         <label class="checkbox task__checkbox">
             <input class="checkbox__input visually-hidden" type="checkbox"  checked >
-            <span class="checkbox__text"><?= $value['task']?></span>
+            <span class="checkbox__text"><?= htmlspecialchars($value['task'])?></span>
         </label>
     </td>
-    <td class="task__date"><?= $value['date']?></td>
+    <td class="task__date"><?= htmlspecialchars($value['date'])?></td>
 
     <td class="task__controls">
     </td>
