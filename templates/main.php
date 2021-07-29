a9bd4261ab497bbbfde67c1a9d6df88cede24c41
Anatoly Pashin <anatoly.pashin@gmail.com>
2021-07-29 11:37:37 +1000

    Update templates/main.php

--- templates/main.php
+++ templates/main.php
@@ -5,7 +5,7 @@
         <ul class="main-navigation__list">
             <?php foreach ($categories as $value): ?>
                 <li class="main-navigation__list-item">
-                    <a class="main-navigation__list-item-link" href="#"><?= $value ?></a>
+                    <a class="main-navigation__list-item-link" href="#"><?= htmlspecialchars($value); ?></a>
                     <span class="main-navigation__list-item-count"><?= countCategory($tasks, $value)  ?></span>
                 </li>
             <?php endforeach; ?>
