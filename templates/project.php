afae99d3920f01fcc45069cf58ccb12239d47e28
Basay <Basay1980@gmail.com>
2021-09-07 15:42:46 +0300

    ДЗ

--- templates/project.php
+++ templates/project.php
@@ -3,9 +3,10 @@
 
     <nav class="main-navigation">
         <ul class="main-navigation__list">
+            <?php $sort = isset($_GET['sort']) ? "&sort=".$_GET['sort'] : "" ?>
             <?php foreach ($projects  as $key=>$value): ?>
                 <li class="main-navigation__list-item <?= $_GET['project_id']==$value["id"] ? " main-navigation__list-item--active " : '' ?>">
-                    <a class="main-navigation__list-item-link" href="http://819795-doingsdone-13/index.php?project_id=<?= $value["id"]; ?>">
+                    <a class="main-navigation__list-item-link" href="http://819795-doingsdone-13/index.php?project_id=<?= $value["id"] . $sort; ?>">
                         <?= htmlspecialchars($value ['content']); ?></a>
                     <span class="main-navigation__list-item-count"><?= countProjects($tasks, $value )  ?></span>
                 </li>
@@ -16,3 +17,4 @@
     <a class="button button--transparent button--plus content__side-button"
        href="add-project.php" target="project_add">Добавить проект</a>
 </section>
+
