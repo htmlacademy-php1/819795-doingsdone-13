60c9d3c2f9978e05e3e2fb22a1925b78920638cd
Basay <Basay1980@gmail.com>
2021-09-14 12:35:05 +0300

    защита

--- notify.php
+++ notify.php
@@ -7,10 +7,11 @@ require ('functions.php');
 
 $usersArray = checkAlarm($link);
 
+
 if (isset($usersArray)) {
 
     foreach ($usersArray as $key => $value) {
-        $message = (new Swift_Message('Wonderful Subject'))
+        $message = (new Swift_Message(' Уведомление от сервиса «Дела в порядке»'))
             ->setFrom(['keks@phpdemo.ru' => 'keks'])
             ->setTo([$value['email'] => $value['name']])
             ->setBody('Уважаемый, ' . $value['name'] . '. У вас запланирована задача ' . $value['content'] . ' на ' . $value['dt_end']);
